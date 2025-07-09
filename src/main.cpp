d91e81367b7e3ffba4ba0a6279dbde5493008805
nils <nils.palme@gmail.com>
2025-07-09 18:15:00 +0200

    checkpoint registry

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,18 @@
 #include "tree/LeafNodeImpl.hpp"
 #include "tree/Builder.hpp"
 #include <memory>
+#include "benchmark/CancelCheckpointRegistry.hpp"
 
 
 int main()
 {
     std::unique_ptr<cancellation::tree::Node> node;
 
-    node = cancellation::tree::Builder<CancelType::kAtomicEnum, 1'000'000'000, 10>::build();
+    node = cancellation::tree::Builder<CancelType::kAtomicEnum, 10'000'000, 10>::build();
 
     node->next();
+
+    cancellation::benchmark::CancelCheckpointRegistry foo;
+    foo.registerExecutionFinished();
 
 }
